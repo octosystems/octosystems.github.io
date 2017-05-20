64e9fd6f7de3bbed393d23ede1455fff34c52702
octosystems <saravana@octosystems.com>
2017-05-20 17:44:17 +0530

    Update Octopus.FashionApp.Shopify.js

--- JS/Octopus.FashionApp.Shopify.js
+++ JS/Octopus.FashionApp.Shopify.js
@@ -212,7 +212,7 @@ var getProduct = function () {
 		  success: getproductcallback
 		});
 	*/	
-	var producturl = constants.shopify_key + ":" + constants.shopify_password + "@https://octosystems.shopify.com/admin/products/" + meta.product.id +".json";
+	var producturl = "https://" + constants.shopify_key + ":" + constants.shopify_password + "@octosystems.shopify.com/admin/products/" + meta.product.id +".json";
 	$.getJSON(producturl,  getproductcallback); 
 	};
 getProduct();
