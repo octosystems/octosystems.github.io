0e8c3cbcd1bfd4c244ab38be92bd238f3dddeec8
octosystems <saravana@octosystems.com>
2017-07-24 12:47:21 +0530

    Update Octopus.FashionApp.Shopify.js

--- JS/Octopus.FashionApp.Shopify.js
+++ JS/Octopus.FashionApp.Shopify.js
@@ -137,6 +137,8 @@ var addbuttontryme= function() {
 	'    <span class="fsnclose" id="spanfsnclose">&times;</span><div>'+
 	'    <label style="color:blue"> Choose image...' + 
 	'    <input type="file" id="myImage" accept="image/*" style="top:-1000px;position:fixed"/></label>' +
+	'    <span id=octPicturemessage style="display:block">MAKE SURE YOUR FACE IS IN THE CENTRE AND IN THE BEST POSSIBLE LIGHT ' +
+	'		WORKS BEST IF YOU ARE WEARING AN OPEN TOP WHILE TAKING THE PICTURE</span>' + 
 	'    <span id="octStatus" style="display:none" ></span>' +
 	'    <img src="about:blank" alt="" id="fsnPhoto" style="width:100%;height:auto; display:none" ></img></div>'+
 	'    <div id="canvasContainer" /></div>'+
